13085c1906d07d7da540c66631131156c216ebbe
Arkenstone <trejo.allan@yahoo.com>
2022-01-21 13:05:25 -0800

    Commit Assignment 3.2 Logging

---
+++ week-3/morgan-logging/trejo-assignment-3.1.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+app.set('views', path.resolve(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+app.use(logger('short'));
+
+app.get('/', function (request, response) {
+  response.render('index', {
+    header: `<h1 style='color: white; font-size: 4rem; font-style: italic'>Cars</h1>`,
+    message: `<table class="pure-table" style="background-color: white; position: absolute; transform: translate(20%,100%)">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Honda</td>
+            <td>Accord</td>
+            <td>2009</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Toyota</td>
+            <td>Camry</td>
+            <td>2012</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Hyundai</td>
+            <td>Elantra</td>
+            <td>2010</td>
+        </tr>
+    </tbody>
+</table>`,
+  });
+});
+
+http.createServer(app).listen(3000, function () {
+  console.log('App started on port 3000');
+});
